18462bb4d634db04727cef3440fa6ab10f43e8a0
joshmas90 <joshmas90@gmail.com>
2026-09-17 12:10:58 -0400

    Add fly control to appointment editor

--- MosquitoNinja/AppointmentEditorViewController.swift
+++ MosquitoNinja/AppointmentEditorViewController.swift
@@ -3,7 +3,7 @@ import UserNotifications
 
 final class AppointmentEditorViewController: NinjaBaseViewController, UITextFieldDelegate, UITextViewDelegate {
     private var appointment: ServiceAppointment?
-    private let serviceControl = UISegmentedControl(items: ["Mosquito", "Ticks", "Both", "Comm/Govt"])
+    private let serviceControl = UISegmentedControl(items: ["Mosquito", "Ticks", "Both", "Comm.", "Flies"])
     private let datePicker = UIDatePicker()
     private let propertyField = UITextField()
     private let notesView = UITextView()
